69cb73fad73a1f329fcdb4155e380c8159820b5e
fkhan313 <farhan.khan615@gmail.com>
2022-08-29 23:54:18 -0400

    Update files

--- src/CopyFile.java
+++ src/CopyFile.java
@@ -4,26 +4,30 @@ import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 public class CopyFile {
 
     public static void main(String[] args) throws IOException {
 
-        Path p1= Paths.get("Test.txt").toAbsolutePath();
+      /*  Path p1= Paths.get("Test.txt").toAbsolutePath();
         System.out.println(p1);
         Path p2= Paths.get("Test1.txt").toAbsolutePath();
-        System.out.println(p2);
+        System.out.println(p2);*/
 
 
 
-        InputStream in = Files.newInputStream(Path.of(args[0]));
-        OutputStream out = Files.newOutputStream(Path.of(args[1]));
+       /* InputStream in = Files.newInputStream(Path.of(args[0]));
+        OutputStream out = Files.newOutputStream(Path.of(args[1]));*/
 
-        /*InputStream in = Files.newInputStream(Path.of("Test.txt"));
-        OutputStream out = Files.newOutputStream(Path.of("Test1.txt"));*/
-        byte[] data= new byte[10];
+        //InputStream in = Files.newInputStream(Path.of("Test"));
+        //OutputStream out = Files.newOutputStream(Path.of("Test1"));
+        InputStream in = Files.newInputStream(Path.of("data"), StandardOpenOption.READ);
+        OutputStream out = Files.newOutputStream(Path.of("data1"));
+        byte[] data= new byte[1024];
         while (in.read(data) !=-1) {
             out.write(data);
+
         }
         in.close();
         out.close();
--- src/Main.java
+++ src/Main.java
@@ -11,10 +11,19 @@ public class Main {
 
         //File & Path API
         Path p = Path.of(".");
-        System.out.println("Path: "+p.toAbsolutePath().normalize().toUri());
+        System.out.println("Path: "+p.toAbsolutePath().normalize().toUri()); //to Uri
 
-        Path p1= Paths.get("Test.txt").toAbsolutePath();
+        Path p1= Paths.get("Test.txt").toAbsolutePath(); //get absolute path of a file
         System.out.println(p1);
+
+        Path currentDir = Paths.get("."); // currentDir = "."
+        Path fullPath = currentDir.toAbsolutePath(); // fullPath = "/Example/example/example"
+        Path one = currentDir.resolve("Test.txt"); // one = "./Test.txt"
+        Path fileName = one.getFileName(); // fileName = "Test.txt"
+        System.out.println(currentDir);
+        System.out.println(fullPath);
+        System.out.println(one);
+        System.out.println(fileName);
 
 
         }
