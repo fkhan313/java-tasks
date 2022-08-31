4539c03337965579133de1cb19359dbeabdb6e89
fkhan313 <farhan.khan615@gmail.com>
2022-08-30 20:21:27 -0400

    Update to multiple files

---
+++ src/FileWriterExample.java
@@ -0,0 +1,20 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterExample {
+
+    public static void main(String[] args) {
+
+        try {
+            FileWriter writer = new FileWriter("poem.txt");
+            writer.write("Roses are red \nViolets are blue \nBooty test test test \nSome text!");
+            writer.append("\n(Line to append)");
+            writer.close();
+        }
+        catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+}
